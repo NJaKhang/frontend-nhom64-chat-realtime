e5e06ce3291c6abf58bb2071e79ea51b91937fa3
NJaKhang <21130392@st.hcmuaf.edu.vn>
2024-05-21 10:58:59 +0700

    add enum socket event

---
+++ src/constants/SocketEvent.ts
@@ -0,0 +1,10 @@
+export enum SocketEvent {
+    Register = "REGISTER",
+    Login = "LOGIN",
+    ReLogin = "RE_LOGIN",
+    Logout = "LOGOUT",
+    CreateRoom = "CREATE_ROOM",
+    JoinRoom = "JOIN_ROOM",
+    SendChat= "SEND_CHAT",
+
+}
