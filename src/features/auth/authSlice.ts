29e928cf6c29b307552789c5420c5cd04be61df4
NJaKhang <21130392@st.hcmuaf.edu.vn>
2024-06-03 15:27:29 +0700

    add redux

--- src/features/auth/authSlice.ts
+++ src/features/auth/authSlice.ts
@@ -1,5 +1,8 @@
-import {User} from "@models/User.ts";
-import {createSlice} from "@reduxjs/toolkit";
+import {Key} from "@constants/Key.ts";
+import chatSlice from "@features/chat/chatSlice.ts";
+import User from "@models/User.ts";
+import {useAppSelector} from "@redux/store.ts";
+import {createSlice, PayloadAction} from "@reduxjs/toolkit";
 
 interface AuthState{
     user: User | null
@@ -16,9 +19,20 @@ const authSlice = createSlice({
     name: "auth",
     initialState,
     reducers: {
-
+        setUser(state, action: PayloadAction<User>){
+            state.user = action.payload
+            localStorage.setItem(Key.USER, JSON.stringify(action.payload))
+        }
     }
 })
+
+export const useAuthAction = () =>{
+    return authSlice.actions;
+}
+
+export const useAuthSelector = () => {
+    return useAppSelector(state => state.auth)
+}
 
 
 export default authSlice
