876cb7b1b8c85677f0b66b432c009bbc4738f8ec
NJaKhang <21130392@st.hcmuaf.edu.vn>
2024-06-03 16:18:55 +0700

    add Login page

--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import LoginPane from "../../layouts/LoginPane";
+
 
 const LoginPage = () => {
-    return (
-        <div>
 
-        </div>
+
+    return (
+        <LoginPane/>
     );
 };
 
