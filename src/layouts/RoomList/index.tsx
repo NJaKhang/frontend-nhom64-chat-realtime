82225bd355fa13c4b270e8487016196fda86a03f
dungpham-programming <21130326@st.hcmuaf.edu.vn>
2024-06-17 16:41:38 +0700

    Thêm UI cho nút "Thêm người dùng" và thêm logic cho dialog của nút

--- src/layouts/RoomList/index.tsx
+++ src/layouts/RoomList/index.tsx
@@ -2,15 +2,26 @@ import {ChatType} from "@constants/ChatType.ts";
 import RoomChat from "@models/RoomChat.ts";
 import GroupOutlinedIcon from '@mui/icons-material/GroupOutlined';
 import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
+import PersonAddIcon from '@mui/icons-material/PersonAdd';
+import GroupAddIcon from '@mui/icons-material/GroupAdd';
 import {TabContext, TabPanel} from "@mui/lab";
 import {
     Avatar,
     Box,
+    Button,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogContentText,
+    DialogTitle,
+    FormControl,
+    IconButton,
+    InputLabel,
     List,
     ListItem,
     ListItemAvatar,
     ListItemButton,
-    ListItemText,
+    ListItemText, MenuItem, OutlinedInput, Select, SelectChangeEvent,
     TextField,
     Typography
 } from "@mui/material";
@@ -24,8 +35,21 @@ const RoomList = () => {
 
     const [value, setValue] = React.useState(ChatType.People);
     const [rooms, setRooms] = useState<RoomChat[]>([])
+    const [openPeople, setOpenPeople] = React.useState(false);
+    const [type, setType] = useState<string>('people');
+
     const handleChange = (event: React.SyntheticEvent, newValue: ChatType) => {
         setValue(newValue);
+    };
+
+    const handleClickOpen = () => {
+        setOpenPeople(true);
+    };
+
+    const handleClose = (event: React.SyntheticEvent<unknown>, reason?: string) => {
+        if (reason !== 'backdropClick') {
+            setOpenPeople(false);
+        }
     };
 
     useEffect(() => {
@@ -46,9 +70,50 @@ const RoomList = () => {
                     Live Chat
                 </Typography>
             </Box>
-            <Box sx={{paddingY: 2}}>
-                <TextField fullWidth size="medium"/>
+
+            <Box sx={{
+                display: "flex"
+            }}>
+                <Box sx={{paddingY: 2}}>
+                    <TextField size="medium"/>
+                </Box>
+
+                <Box sx={{
+                    paddingY: 2,
+                    marginLeft: "8px"
+                }}>
+                    <Box>
+                        <IconButton color="primary" onClick={handleClickOpen} sx={{padding: "14px"}}>
+                            <PersonAddIcon color="primary"/>
+                        </IconButton>
+                        <Dialog
+                            open={openPeople}
+                            onClose={handleClose}
+                            aria-labelledby="alert-dialog-title"
+                            aria-describedby="alert-dialog-description"
+                        >
+                            <DialogTitle id="alert-dialog-title" sx={{
+                                fontWeight: "bold",
+                                fontSize: "20px"
+                            }}>
+                                {"Thêm người dùng mới"}
+                            </DialogTitle>
+                            <DialogContent>
+                                <DialogContentText id="alert-dialog-description" sx={{paddingTop: "10px"}}>
+                                    <TextField id="outlined-basic" label="Thêm người dùng" variant="outlined" sx={{width: 300}}/>
+                                </DialogContentText>
+                            </DialogContent>
+                            <DialogActions>
+                                <Button onClick={handleClose}>Disagree</Button>
+                                <Button onClick={handleClose} autoFocus>
+                                    Agree
+                                </Button>
+                            </DialogActions>
+                        </Dialog>
+                    </Box>
+                </Box>
             </Box>
+
             <Box sx={{flex: 1}}>
                 <TabContext value={value}>
                     <Tabs value={value} onChange={handleChange} aria-label="icon tabs example" variant="fullWidth">
@@ -58,7 +123,7 @@ const RoomList = () => {
                     </Tabs>
                     <TabPanel value={ChatType.People} sx={{padding: 0}}>
                         <List sx={{width: '100%', maxWidth: 360, bgcolor: 'background.paper'}}>
-                            {rooms.map((room)=> <RoomItem data={room} chatType={value} key={room.name}/>)}
+                            {rooms.map((room) => <RoomItem data={room} chatType={value} key={room.name}/>)}
                         </List>
                     </TabPanel>
                     <TabPanel value={ChatType.Group}>Item Two</TabPanel>
